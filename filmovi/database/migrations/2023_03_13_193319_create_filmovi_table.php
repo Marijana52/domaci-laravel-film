1b417e67a159fbc2160d440c858da3454546f8e9
Marijana <makizaric52@gmail.com>
2023-03-13 20:35:36 +0100

    Migracije za kreiranje tabela filmovi,gledaoci...

---
+++ filmovi/database/migrations/2023_03_13_193319_create_filmovi_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('filmovi', function (Blueprint $table) {
+            $table->id();
+            $table->string('naziv',20);
+            $table->integer('brojSale');
+            $table->string('nacinPrevodjenja');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filmovi');
+    }
+};
